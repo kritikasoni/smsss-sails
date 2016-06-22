277d39d63e992fd05b1e21688761e4d520bae4f0
kris <kritika_soni@cmu.ac.th>
2016-06-22 20:49:08 +0700

    create role and department api,rename doctor to staff

--- api/models/Staff.js
+++ api/models/Staff.js
@@ -1,12 +1,12 @@
 /**
- * Doctor.js
+ * Staff.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
-
+  tableName: 'staffs',
   attributes: {
     firstName: {
       type: 'string'
@@ -14,8 +14,21 @@ module.exports = {
     lastName: {
       type: 'string'
     },
-    department:{
+    email:{
+      type:'string',
+      unique:true
+    },
+    password:{
       type:'string'
+    },
+    position:{
+      type:'string'
+    },
+    roleId:{
+      model:'role'
+    },
+    departmentId:{
+      model:'department'
     }
   }
 };
