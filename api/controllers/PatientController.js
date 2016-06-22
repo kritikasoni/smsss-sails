e63a7a0ae8ea1c617e01d8cd305fcdcc7a36a419
kris <kritika_soni@cmu.ac.th>
2016-06-22 20:02:36 +0700

    Initial commit

---
+++ api/controllers/PatientController.js
@@ -0,0 +1,11 @@
+/**
+ * PatientController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
